00793236a2d522cfcaa47e867bd211c9d72083d8
geki7 <83777643+geki7@users.noreply.github.com>
2023-05-24 00:24:39 +0200

    Početna
    Izrada početne forme

--- CoffeeApp/CoffeeApp/frmPrijava.cs
+++ CoffeeApp/CoffeeApp/frmPrijava.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace CoffeeApp
 {
-    public partial class Form1 : Form
+    public partial class frmPrijava : Form
     {
-        public Form1()
+        public frmPrijava()
         {
             InitializeComponent();
         }
