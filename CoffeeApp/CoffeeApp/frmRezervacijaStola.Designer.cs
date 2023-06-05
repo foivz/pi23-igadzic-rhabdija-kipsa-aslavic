02e22cdbd21ce0ebc4a9cc969453dae671ed1444
weis25 <slavicandrija@gmail.com>
2023-06-05 19:42:54 +0200

    Rezervacija stola
    Ponovno napravljen dizajn forme tervacije stola radi problema sa sinkronizacijom githuba i visual studija

---
+++ CoffeeApp/CoffeeApp/frmRezervacijaStola.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace CoffeeApp
+{
+    partial class frmRezervacijaStola
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnRezerviraj = new System.Windows.Forms.Button();
+            this.btnPonisti = new System.Windows.Forms.Button();
+            this.btnIzlaz = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 55);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(592, 375);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnRezerviraj
+            // 
+            this.btnRezerviraj.Location = new System.Drawing.Point(646, 55);
+            this.btnRezerviraj.Name = "btnRezerviraj";
+            this.btnRezerviraj.Size = new System.Drawing.Size(154, 105);
+            this.btnRezerviraj.TabIndex = 1;
+            this.btnRezerviraj.Text = "Rezerviraj stol";
+            this.btnRezerviraj.UseVisualStyleBackColor = true;
+            // 
+            // btnPonisti
+            // 
+            this.btnPonisti.Location = new System.Drawing.Point(646, 177);
+            this.btnPonisti.Name = "btnPonisti";
+            this.btnPonisti.Size = new System.Drawing.Size(154, 94);
+            this.btnPonisti.TabIndex = 2;
+            this.btnPonisti.Text = "Poništi rezervaciju";
+            this.btnPonisti.UseVisualStyleBackColor = true;
+            // 
+            // btnIzlaz
+            // 
+            this.btnIzlaz.Location = new System.Drawing.Point(646, 332);
+            this.btnIzlaz.Name = "btnIzlaz";
+            this.btnIzlaz.Size = new System.Drawing.Size(154, 98);
+            this.btnIzlaz.TabIndex = 3;
+            this.btnIzlaz.Text = "Izlaz";
+            this.btnIzlaz.UseVisualStyleBackColor = true;
+            // 
+            // frmRezervacijaStola
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(852, 494);
+            this.Controls.Add(this.btnIzlaz);
+            this.Controls.Add(this.btnPonisti);
+            this.Controls.Add(this.btnRezerviraj);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "frmRezervacijaStola";
+            this.Text = "RezervacijaStola";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnRezerviraj;
+        private System.Windows.Forms.Button btnPonisti;
+        private System.Windows.Forms.Button btnIzlaz;
+    }
+}
